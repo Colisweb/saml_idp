afab1381e0a7a1eff4f23bd7b2b8d32a75e33ecd
FlorianDoublet <florian.doublet@hotmail.fr>
2017-06-20 15:49:28 +0200

    Remove layout to false

--- app/controllers/saml_idp/idp_controller.rb
+++ app/controllers/saml_idp/idp_controller.rb
@@ -22,7 +22,7 @@ module SamlIdp
           @saml_idp_fail_msg = "Incorrect email or password."
         else
           @saml_response = idp_make_saml_response(person)
-          render :template => saml_idp_post_template_path, :layout => false
+          render :template => saml_idp_post_template_path
           return
         end
       end
@@ -32,7 +32,7 @@ module SamlIdp
     def logout
       idp_logout
       @saml_response = idp_make_saml_response(nil)
-      render :template => saml_idp_post_template_path, :layout => false
+      render :template => saml_idp_post_template_path
     end
 
     def idp_logout
